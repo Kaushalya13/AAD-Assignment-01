8b86981f26ce2d693c4c6d05cbe18a3bd0899658
Kaushalya13 <nimanthikaushalya13@gmail.com>
2024-01-16 22:39:18 +0530

    Updating

--- src/main/java/lk/ijse/aad_assignment_01/api/Customer.java
+++ src/main/java/lk/ijse/aad_assignment_01/api/Customer.java
@@ -7,12 +7,31 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.aad_assignment_01.db.CustomerDB;
 import lk.ijse.aad_assignment_01.dto.CustomerDTO;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet(name = "customer",urlPatterns = "/customer")
 public class Customer extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        try {
+            InitialContext ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/pos");
+            this.connection = pool.getConnection();
+        } catch (SQLException | NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    Connection connection;
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Jsonb jsonb = JsonbBuilder.create();
@@ -22,5 +41,13 @@ public class Customer extends HttpServlet {
         System.out.println(customerDTO.getName());
         System.out.println(customerDTO.getNic());
         System.out.println(customerDTO.getAddress());
+
+
+        CustomerDB customerDB = new CustomerDB();
+
+        if (customerDB.saveCustomer(customerDTO,connection)){
+            PrintWriter writer = resp.getWriter();
+            writer.write("Customer Saved ...");
+        }
     }
 }
--- src/main/java/lk/ijse/aad_assignment_01/db/CustomerDB.java
+++ src/main/java/lk/ijse/aad_assignment_01/db/CustomerDB.java
@@ -1,4 +1,33 @@
 package lk.ijse.aad_assignment_01.db;
 
+import lk.ijse.aad_assignment_01.dto.CustomerDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class CustomerDB {
+    final static Logger logger = LoggerFactory.getLogger(CustomerDB.class);
+
+    public boolean saveCustomer(CustomerDTO customerDTO, Connection connection){
+        try {
+            var ps = connection.prepareStatement("INSERT INTO customer(cus_id,name,nic,address) VALUES (?,?,?,?)");
+            ps.setString(1,customerDTO.getCus_id());
+            ps.setString(2,customerDTO.getName());
+            ps.setString(3,customerDTO.getNic());
+            ps.setString(4,customerDTO.getAddress());
+
+            if (ps.executeUpdate() != 0) {
+                logger.info("Customer Data saved");
+                return true;
+            } else {
+                logger.error("Customer Data not saved");
+                return false;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
